d67e793536494b3eb7d2a818cbcfd191bb4ca6cf
Robson <rma@a.palmares.ifpe.edu.br>
2023-08-20 16:11:38 -0300

    aula15 - ambiente #22

---
+++ aula015/ambiente.js
@@ -0,0 +1,16 @@
+var num = [5, 8, 2, 9, 3]
+num.push(1)
+num.sort()
+console.log(num)
+console.log(`Nosso vetor tem ${num.length} posições`)
+console.log(`O primeiro valor do vetor é ${num[0]}`)
+
+for(var pos = 0; pos < num.length; pos++) {
+    console.log(`${pos}`)
+}
+var pos = num.indexOf(8)
+if (pos == -1) {
+    console.log('O valor não foi encontrado!')
+} else {
+    console.log(`O valor 8 está na posição ${pos}`)
+}
