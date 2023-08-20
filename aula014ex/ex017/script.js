62ac7f84a9ac853d2434c51d5cd30e12e6ea42af
Robson <rma@a.palmares.ifpe.edu.br>
2023-08-20 15:25:45 -0300

    aula14ex - ex017 #21

---
+++ aula014ex/ex017/script.js
@@ -0,0 +1,25 @@
+function tabuada() {
+    var num = document.getElementById('txtn')
+    var tab = document.getElementById('seltab')
+    if (num.value.length == 0) {
+        alert('Por favor, digite um número!')
+    } else {
+        var n = Number(num.value)
+        tab.innerHTML = ''
+        for(var c = 1; c <= 10; c+=1) {
+            var item = document.createElement('option')
+            item.text = `${n} x ${c} = ${n*c}`
+            item.value = `tab${c}`
+            tab.appendChild(item)
+        }
+        /*var c = 1
+        while (c <=10) {
+            var item = document.createElement('option')
+            item.text = `${n} x ${c} = ${n*c}`
+            item.value = `tab${c}`
+            tab.appendChild(item)
+            c++
+        }*/
+    }   
+
+}
